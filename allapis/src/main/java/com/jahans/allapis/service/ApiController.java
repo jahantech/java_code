34f54abf637f9e37a81078d9b04dec920c507d21
jahantech <jahansyed@hotmail.co.uk>
2018-01-14 00:42:36 +0000

    Working function

--- allapis/src/main/java/com/jahans/allapis/service/ApiController.java
+++ allapis/src/main/java/com/jahans/allapis/service/ApiController.java
@@ -3,12 +3,19 @@ package com.jahans.allapis.service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Random;
+
 @RestController
 public class ApiController {
 
     @RequestMapping("/randomNumber")
     public int randomNumFunc(){
 
-        return 2;
+
+        Random rand = new Random();
+
+        int  n = rand.nextInt(1000000) + 1;
+
+        return n;
     }
 }
